5ef9536f7f5df318849deb9f4b33e5359e150f6e
Justin-Insight <jparsons@insightcreative.com>
2019-01-15 14:53:19 -0600

    initial commit to set up the foundation for our new theme

---
+++ assets/scss/wp-content2/themes/ardensAutoResponsiveChild/footer.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package ardensAutoResponsive
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+
+		<div class="site-info pageWidth">
+
+			<li>
+
+				<a class="footerLink" href = "https://expressrecycling.biz/acceptable-materials/">Auto Repair Services</a>
+
+			</li>
+
+			<li>
+
+				<a class="footerLink" href = "https://expressrecycling.biz/recycling-services/">Projects</a>
+
+			</li>
+
+			<li>
+
+				<a class="footerLink" href = "https://expressrecycling.biz/recycling-services/document-shredding/">About</a>
+
+			</li>
+
+			<li>
+
+				<a class="footerLink" href = "https://expressrecycling.biz/about/">Promotions</a>
+
+			</li>
+
+			<li>
+
+				<a class="footerLink" href = "https://expressrecycling.biz/contact/">Contact</a>
+
+			</li>
+
+		</div><!-- .site-info -->
+
+		<div class="footerContact pageWidth flex-container">
+
+			<div class="col50 centerText">
+
+			<a href="https://expressrecycling.biz"><img src="http://localhost/ardens/assets/scss/wp-content/uploads/2019/01/ArdensLogoSmall.png" class="footerLogo" ></a>
+
+			<h3 class="centerText">Ardens Automotive Hospital</h3>
+
+			<a target="_blank" href="https://www.google.com/maps/dir/''/Express+Recycling+Solutions,+Inc.,+5949,+480+Strodman+Ave,+Wisconsin+Rapids,+WI+54494/data=!4m5!4m4!1m0!1m2!1m1!1s0x880088481020408f:0xb96b50ee5af1c418?sa=X&ved=0ahUKEwiivIGkxpTcAhUKW60KHbsgB40QiBMILDAA"><p class="centerText">510 W 8th Ave. Oshkosh, WI 54902</p></a>
+
+			<a class="whiteText centerText" href="tel:(920)203-3694"><p>(920)203-3694</p></a>
+
+			<i class="redIcon fab fa-facebook-square"></i>
+
+			</div>
+
+		</div>
+
+			<div id="copyright">
+
+				<div class="copyrightContentWrap navWidth">
+
+					<div id="copyrightWrap">
+
+						<div id="copyrightContent">
+
+							<i id="footerIcon" class="whiteIcon far fa-copyright"></i><p>&nbsp;<script>document.write(new Date().getFullYear());</script> Ardens Automotive Hospital</p>
+
+						</div>
+
+						<div id="closingLinks">
+
+							<a href="https://expressrecycling.biz/sitemap/" class="borderRight">Sitemap</a> |
+
+							<a href="https://expressrecycling.biz/privacy-policy/" class="borderRight ">Privacy Policy</a> |
+
+							<a href="https://expressrecycling.biz/contact/" id="footerContactLink">Contact Us</a>
+
+						</div>
+
+					</div>
+
+					<div class="siteAuthor">Web Design by
+
+							<a target="_blank" href="https://seejustincode.com"> Justin Parsons</a>
+
+					</div>
+
+				</div>
+
+			</div>
+			<!-- Google Tag Manager (noscript)
+			<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TPJZV49"
+			height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+			 End Google Tag Manager (noscript) -->
+
+			<!-- Script for barba.js-->
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/barba.js/1.0.0/barba.min.js" type="text/javascript"></script>
+
+			<!--Script for rellax-->
+			<script src="/ardens/assets/scss/wp-content/themes/ardensAutoResponsiveChild/JS/rellax.min.js" type="text/javascript"></script>
+	</footer><!-- #colophon -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
---
+++ assets/scss/wp-content2/themes/ardensAutoResponsiveChild/header.php
@@ -0,0 +1,199 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package ardensAutoResponsive
+ */
+?>
+<!doctype html>
+
+<html <?php language_attributes(); ?>>
+
+<head>
+
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<link href="http://localhost/ardens/assets/scss/wp-content/themes/ardensAutoResponsiveChild/Assets/webfonts/fontawesome-all.css" rel="stylesheet">
+
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="  crossorigin="anonymous"></script>
+
+	<!--Scripte for rellax-->
+	<script src="http://localhost/ardens/assets/scss/wp-content/themes/ardensAutoResponsiveChild/JS/rellax.min.js" type="text/javascript"></script>
+
+	<?php wp_head(); ?>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<!--
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110471650-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-110471650-1');
+	</script>
+-->
+
+	<!-- Google Tag Manager -->
+	<!--
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-TPJZV49');</script>
+-->
+	<!-- End Google Tag Manager -->
+</head>
+
+<body <?php body_class(); ?>>
+
+<div class = "menu">
+
+		<div class = "linkWrap">
+
+			<ul class="linkList">
+
+				<li>
+
+					<a class="menuLink" href = "/ardens/auto-repair-services/">Auto Repair Services</a><i class=" redIcon fas fa-chevron-right"></i>
+
+				</li>
+
+				<li>
+
+					<a class="menuLink" href = "/ardens/projects/">Projects</a><i class=" redIcon fas fa-chevron-right"></i>
+
+				</li>
+
+				<li>
+
+					<a class="menuLink" href = "/ardens/about/">About</a><i class=" redIcon fas fa-chevron-right"></i>
+
+				</li>
+
+				<li>
+
+					<a class="menuLink" href = "/ardens/promotions/">Promotions</a><i class=" redIcon fas fa-chevron-right"></i>
+
+				</li>
+
+				<li>
+
+					<a class="menuLink" href = "/ardens/contact/">Contact</a><i class=" redIcon fas fa-chevron-right"></i>
+
+				</li>
+
+			</ul>
+
+		</div>
+
+		<div class="closeMenuButton fullWidth centeredContainer">
+
+			<div>
+
+				<h4 id="closeMenuTitle">Close Menu</h4>
+
+			</div>
+
+		</div>
+
+</div>
+
+<div id="page" class="site">
+
+	<header id="masthead" class="site-header" role="navigation">
+
+		<div class="secondaryNavigation">
+
+			<div class="navWidth secondaryMenuWrap">
+
+				<i class="fas fa-map-marker-alt whiteIcon"></i>
+
+				<a class="secondaryLink" href="mailto:email@expressrecycling.biz">Map</a>
+
+				<i class="fas fa-phone whiteIcon"></i>
+
+				<a class="secondaryLink" href="tel:(920)203-3694">(920)203-3694</a>
+
+			</div>
+
+		</div>
+
+		<div id="site-navigation" class="main-navigation">
+
+			<nav>
+
+				<div class="navWidth menuLinkWrap">
+
+						<a href="/ardens/"><img src="http://localhost/ardens/assets/scss/wp-content/uploads/2019/01/ArdensLogoSmall.png" class="siteLogo" ></a>
+
+						<ul class="linkList2">
+
+							<li>
+
+								<a class="menuLink" href = "/ardens/auto-repair-services/">Auto Repair Services</a>
+
+							</li>
+
+							<li>
+
+								<a class="menuLink" href = "/ardens/projects/">Projects</a>
+
+							</li>
+
+							<li>
+
+								<a class="menuLink" href = "/ardens/about/">About</a>
+
+							</li>
+
+							<li>
+
+								<a class="menuLink" href = "/ardens/promotions/">Promotions</a>
+
+							</li>
+
+							<li>
+
+								<a class="redButton" href = "/ardens/contact/">Contact</a>
+
+							</li>
+
+						</ul>
+
+						<div class = "menuButtonWrap">
+
+							<div class = "menuTextWrap">
+
+								<span class = "menuText">Menu</span>
+
+								<span class = "menuBars">
+
+									<span></span>
+
+									<span></span>
+
+									<span></span>
+								</span>
+
+							</div>
+
+						</div>
+
+				</div>
+
+			</nav>
+
+		</div><!-- #site-navigation -->
+
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
